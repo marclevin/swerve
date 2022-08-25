cbbe95f44c179066d4902d94ae699d628e2342db
Marc <marclevin.sa@gmail.com>
2022-08-25 14:06:33 +0200

    Created Skeleton Project

---
+++ store/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'index.html')
+
+
+def store(request):
+    return render(request, 'store.html')
