b312a8636754bef0546fd81223371380c6f8a4c9
Joel Haag <joelohaag@gmail.com>
2022-08-27 16:32:02 +0200

    models- creating Invoice edit and function

--- store/models.py
+++ store/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -34,6 +36,20 @@ class Product(models.Model):
     stock = models.IntegerField()
     image = models.ImageField(upload_to='static/Images')
 
+    @staticmethod
+    def get_products_by_id(ids):
+        return Product.objects.filter(id__in=ids)
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
+    @staticmethod
+    def get_all_products_by_category(category_id):
+        if category_id:
+            return Product.objects.filter(category=category_id)
+        else:
+            return Product.get_all_products()
 
     def __str__(self):
         return self.name
@@ -43,5 +59,11 @@ class Invoice(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    status = models
+    destination = models.CharField(max_length=50, default='', blank=True)
+    price = models.IntegerField(default=0)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
 
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Invoice.objects.filter(customer=customer_id).order_by('-date')
