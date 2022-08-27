61615e7457bc7c1b1bfca4955904b8c1c624e686
Joel Haag <joelohaag@gmail.com>
2022-08-27 15:09:15 +0200

    models- creating Invoice, adding to other models too

--- store/models.py
+++ store/models.py
@@ -6,6 +6,7 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=100, blank=True)
     address = models.CharField(max_length=200, blank=True)
+    contact = models.CharField(max_length=24, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -13,8 +14,12 @@ class Customer(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=350)
-    image = models.ImageField(upload_to='static/Images')
+    description = models.CharField(max_length=350, blank=True)
+    image = models.ImageField(upload_to='static/Images', default='afk.jpg')
+
+    @staticmethod
+    def get_categories():
+        return Category.objects.all()
 
     def __str__(self):
         return self.name
@@ -29,5 +34,14 @@ class Product(models.Model):
     stock = models.IntegerField()
     image = models.ImageField(upload_to='static/Images')
 
+
     def __str__(self):
         return self.name
+
+
+class Invoice(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    status = models
+
